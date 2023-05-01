922a614419f138e834cae147d6a5e3db34cbbf24
wjddns0122 <jwoon2004@gmail.com>
2023-05-01 19:15:24 +0900

    finish

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // 앱에서 표시할 url 입력
+        webView.loadUrl("https://m.soreaav.co.kr/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    //폰의 뒤로가기 버튼의 동작 입력
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/webview/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.webview;
+
+//여기서부터 수정
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            // 웹뷰가 표시되기 전에 스플래시가 표시될 시간. ms단위
+        }, 4000);
+    }
+}
